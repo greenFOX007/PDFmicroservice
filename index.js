ac8ef92c29c14302dc30513c2c7986f3b79829e6
Vladimir Mihdalionak <vova280297@gmail.com>
2024-05-17 09:55:16 +0300

    sdfdsf

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 const cors = require("cors");
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
