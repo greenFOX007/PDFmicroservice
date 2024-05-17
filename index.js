437b287e710ed1602d2ddd9d90116d7c66792f64
Vladimir Mihdalionak <vova280297@gmail.com>
2024-05-17 10:10:57 +0300

    dsfsdfsdf

--- index.js
+++ index.js
@@ -14,32 +14,34 @@ app.get("/lol", async (req, res) => {
 app.post("/create-pdf", async (req, res) => {
   const htmlContent = req.body.html;
 
-  try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  res.send(htmlContent);
 
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+  // try {
+  //   const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
 
-    const pdfBuffer = await page.pdf({
-      format: "A4",
-      printBackground: true,
-      margin: {
-        top: "30px",
-        right: "30px",
-        bottom: "30px",
-        left: "30px",
-      },
-    });
+  //   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
 
-    await browser.close();
+  //   const pdfBuffer = await page.pdf({
+  //     format: "A4",
+  //     printBackground: true,
+  //     margin: {
+  //       top: "30px",
+  //       right: "30px",
+  //       bottom: "30px",
+  //       left: "30px",
+  //     },
+  //   });
 
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Length", pdfBuffer.length);
-    res.send(pdfBuffer);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error generating PDF");
-  }
+  //   await browser.close();
+
+  //   res.setHeader("Content-Type", "application/pdf");
+  //   res.setHeader("Content-Length", pdfBuffer.length);
+  //   res.send(pdfBuffer);
+  // } catch (error) {
+  //   console.error(error);
+  //   res.status(500).send("Error generating PDF");
+  // }
 });
 
 const PORT = process.env.PORT || 3001;
